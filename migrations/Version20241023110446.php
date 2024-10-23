af3a80df166055dff9eaa2a9610c8528499762ed
Ftk-keit <fatima.keita@ism.edu.sn>
2024-10-23 11:33:28 +0000

    entity and migration client

---
+++ migrations/Version20241023110446.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20241023110446 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP SEQUENCE article_id_seq CASCADE');
+        $this->addSql('DROP TABLE article');
+        $this->addSql('ALTER TABLE client ADD adresse VARCHAR(25) NULL');
+        $this->addSql('ALTER TABLE client ALTER telephone TYPE VARCHAR(14)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('CREATE SEQUENCE article_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE TABLE article (id SERIAL NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('ALTER TABLE client DROP adresse');
+        $this->addSql('ALTER TABLE client ALTER telephone TYPE VARCHAR(25)');
+    }
+}
